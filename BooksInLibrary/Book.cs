65e0d61b2515a8e80f0fd443a6a0c9fa4c3e38da
STEM-1-13 <STEM-1-13@DESKTOP-BLJBVM5>
2024-11-06 11:30:44 +0200

    Add project files.

---
+++ BooksInLibrary/Book.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksInLibrary
+{
+    public class Book
+    {
+        private string name;
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		private string avtor;
+
+		public string Avtor
+		{
+			get { return avtor; }
+			set
+			{
+				if (value.Length < 2)
+				{
+					throw new ArgumentException("Nqma takuv avtor!");
+				}
+				else
+				{ 
+					avtor = value;	
+				}
+			}
+		}
+		private string izdatelstvo;
+
+		public string Izdatelstvo
+		{
+			get { return izdatelstvo; }
+			set { izdatelstvo = value; }
+		}
+		private int age;
+
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+
+        public Book(string name)
+        {
+            this.Name = name;
+        }
+        public Book(string name,string avtor,string izdatelstvo,int age)
+        {
+			this.name = name;
+			this.avtor = avtor;
+			this.izdatelstvo = izdatelstvo;
+            this.age = age;	
+        }
+		public void PrintName()
+		{
+            Console.WriteLine($"The book name is {this.name} !");
+        }
+		public void PrintAllOfThem()
+		{
+            Console.WriteLine($"Imeto na knigata e {this.name} s avtor {this.avtor} na izdanie {this.izdatelstvo} otpechatana prez {age}. ");
+        }
+    }
+}
---
+++ BooksInLibrary/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksInLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Book book1 = new Book("Pod igoto");
+                book1.PrintName();
+
+                Console.WriteLine("----------------------------------------------");
+
+                Console.Write("Vavedi n = ");
+                int n = int.Parse(Console.ReadLine());
+
+                List<Book> books = new List<Book>();
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write($"Vavedi dannite:");
+                    var danni = Console.ReadLine().Split().ToList();
+                    Book book = new Book(danni[0], danni[1], danni[2], int.Parse(danni[3]));
+                    books.Add(book);
+                }
+
+                books.ForEach(x => x.PrintAllOfThem());
+
+                Console.WriteLine("------------------------------------");
+
+                books.OrderBy(x => x.Age).First().PrintAllOfThem();
+
+                Console.WriteLine("------------------------------------");
+
+                books.OrderBy(x => x.Name).ToList().ForEach(book => book.PrintAllOfThem());
+
+                Console.WriteLine("-------------------------------------");
+
+                Console.Write("Vavedi avtor: ");
+                string avtor = Console.ReadLine();
+
+                books.Where(x=>x.Avtor==avtor).ToList().ForEach(x => x.PrintAllOfThem());
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
